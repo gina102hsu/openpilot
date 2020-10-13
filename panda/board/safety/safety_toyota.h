81dec36d733e681c3e84047ac9f8652819a6f921
gina102hsu <58519834+gina102hsu@users.noreply.github.com>
2020-10-13 09:33:01 +0800

    Enable ui

--- panda/board/safety/safety_toyota.h
+++ panda/board/safety/safety_toyota.h
@@ -225,7 +225,7 @@ static int toyota_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
        if (stop_forward_steer==0)
        {
           eon_counter++;
-          if (eon_counter>20)   // 20*50ms, 1s. prevent switch between stock and eon too freqently 
+          if (eon_counter>5)   // 5*50ms, 250ms. prevent switch between stock and eon too freqently 
           {
              stop_forward_steer = 1;
           }
@@ -313,7 +313,7 @@ static int toyota_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
       int addr = GET_ADDR(to_fwd);
       // block stock lkas messages and stock acc messages (if OP is doing ACC)
       // in TSS2, 0x191 is LTA which we need to block to avoid controls collision
-      int is_lkas_msg = ((addr == 0x2E4) /*|| (addr == 0x412)*/ || (addr == 0x191));
+      int is_lkas_msg = ((addr == 0x2E4) || (addr == 0x412) || (addr == 0x191));
       if (stop_forward_steer==0)
         is_lkas_msg=0;
       // in TSS2 the camera does ACC as well, so filter 0x343
